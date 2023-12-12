6e6f10609ccad8e5a01c8475743f663d571f5b6f
Jonax <jonaxwork@outlook.com>
2023-12-12 16:36:33 +0800

    app seesion add

--- myapp/routes.go
+++ myapp/routes.go
@@ -9,8 +9,12 @@ import (
 func (a *application) routes() *chi.Mux {
 	// middleware must come before any routes
 
+
 	// add routes here
 	a.App.Routes.Get("/", a.Handlers.Home)
+	a.App.Routes.Get("/go-page", a.Handlers.GoPage)
+	a.App.Routes.Get("/jet-page", a.Handlers.JetPage)
+	a.App.Routes.Get("/sessions", a.Handlers.SessionTest)
 
 	// static routes
 	fileServer := http.FileServer(http.Dir("./public"))
