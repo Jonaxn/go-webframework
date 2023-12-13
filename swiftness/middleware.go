08c4d683e15d7247617bac82881ca331deadc5b2
Jonax <jonaxwork@outlook.com>
2023-12-13 11:51:37 +0800

    support for postgres

--- swiftness/middleware.go
+++ swiftness/middleware.go
@@ -3,6 +3,5 @@ package swiftness
 import "net/http"
 
 func (c *Swiftness) SessionLoad(next http.Handler) http.Handler {
-	c.InfoLog.Println("SessionLoad called")
 	return c.Session.LoadAndSave(next)
 }
