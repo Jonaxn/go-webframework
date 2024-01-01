d9cf476115d97a6d2e2364de2a5b4caa57955316
Jonax <jonaxwork@outlook.com>
2024-01-01 10:24:26 +0800

    skeleton

--- swiftness/cmd/cli/new.go
+++ swiftness/cmd/cli/new.go
@@ -23,7 +23,7 @@ func doNew(appName string) {
 	// git clone the skeleton application
 	color.Green("\tCloning repository...")
 	_, err := git.PlainClone("./"+appName, false, &git.CloneOptions{
-		URL:      "git@github.com/Jonaxn/swiftness-app.git",
+		URL:      "git@github.com:Jonaxn/swiftness-app.git",
 		Progress: os.Stdout,
 		Depth:    1,
 	})
